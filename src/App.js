7a5805f5c5e9ba4c143fd0bde5bac32e0953016c
Chris Kapend <chriskapend@gmail.com>
2020-04-26 21:51:42 +0200

    initial files creations

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navigation from './components/navigation/Navigation';
+
+class App extends Component {
+    render() {
+        return (
+            <section>
+                <Navigation />
+                
+            </section>
+        )
+    }
+}
+export default App;
---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function App() {
+    return (
+        <nav>
+            <header>
+
+            </header>
+            <ul>
+                <li>
+                    <a href="#">Computers</a>
+                </li>
+                <li>
+                    <a href="#">Telephones</a>
+                </li>
+                <li>
+                    <a href="#">Users</a>
+                </li>
+                <li>
+                    <a href="#">Servers</a>
+                </li>
+                <li>
+                    <a href="#">Routers & Switches</a>
+                </li>
+                <li>
+                    <a href="#">Wireless Devices</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default App;
